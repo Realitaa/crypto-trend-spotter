26c8858fcfdf3ef29878ed4340ff1f3193214f17
rezamuliaputra927 <rezamuliaputra927@gmail.com>
2025-11-29 16:17:55 +0000

    feat: add nuxt/supabase library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui', 'nuxt-tradingview', '@nuxt/content'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', 'nuxt-tradingview', '@nuxt/content', '@nuxtjs/supabase'],
   css: ['@/assets/css/main.css'],
   colorMode: {
     preference: 'system',
