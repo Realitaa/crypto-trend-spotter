aaf104038752234ab7cbf35e08696530f43db96a
rezamuliaputra927 <rezamuliaputra927@gmail.com>
2025-12-09 16:42:16 +0000

    style: fix responsivity

--- app/components/AppNavbar.vue
+++ app/components/AppNavbar.vue
@@ -16,10 +16,29 @@ const coins = computed(() => [
 ])
 
 // time
-const utcNow = ref('--:--:--')
+const utcHH = ref('--')
+const utcMM = ref('--')
+const showColon = ref(true)
+const desktopTime = ref('--:--:--')
 
 const updateTime = () => {
-  utcNow.value = new Date().toLocaleString('en-GB', {
+  const now = new Date()
+
+  // mobile (HH:MM saja)
+  utcHH.value = now.toLocaleString('en-GB', {
+    timeZone: 'UTC',
+    hour12: false,
+    hour: '2-digit',
+  })
+  utcMM.value = now.toLocaleString('en-GB', {
+    timeZone: 'UTC',
+    hour12: false,
+    minute: '2-digit',
+  })
+  showColon.value = !showColon.value
+
+  // desktop (HH:MM:SS)
+  desktopTime.value = now.toLocaleString('en-GB', {
     timeZone: 'UTC',
     hour12: false,
     hour: '2-digit',
@@ -33,18 +52,32 @@ onMounted(() => {
   setInterval(updateTime, 1000)
 })
 
+onMounted(() => {
+  updateTime()
+  setInterval(updateTime, 1000)
+})
+
 </script>
 
 <template>
   <UDashboardNavbar :ui="{ right: 'gap-3' }">
     <template #title>
       <NuxtLink to="/" class="font-semibold text-xl hover:opacity-80 transition">
-        Crypto Trend Spotter
+        <span class="sm:hidden">CTS</span>
+        <span class="hidden sm:inline">Crypto Trend Spotter</span>
       </NuxtLink>
     </template>
 
     <template #right>
-      <span class="text-sm cursor-default">Waktu: {{ utcNow }} UTC</span>
+      <!-- Desktop: jam:menit:detik -->
+        <div class="hidden sm:block text-sm">
+          Waktu: {{ desktopTime }} UTC
+        </div>
+
+      <!-- Mobile: hanya jam & menit, titik dua berkedip -->
+        <div class="block sm:hidden text-sm">
+          {{ utcHH }}<span :style="{ opacity: showColon ? 1 : 0 }">:</span>{{ utcMM }}
+        </div>
 
       <BinanceStatus />
 
